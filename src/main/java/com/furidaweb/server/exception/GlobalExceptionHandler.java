e7a2fd230309edaf7d9223d6fd6deda6c98022f4
amankr8 <amankumar141099@gmail.com>
2024-11-11 23:19:42 +0530

    Handle exception for MaxUploadSizeExceeded for file size exceeded limit

--- src/main/java/com/furidaweb/server/exception/GlobalExceptionHandler.java
+++ src/main/java/com/furidaweb/server/exception/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.furidaweb.server.exception;
+import com.furidaweb.server.dto.StatusResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,8 +23,9 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<?> handleResourceNotFound(ResourceNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<?> handleMaxSizeException(MaxUploadSizeExceededException ex) {
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE)
+                .body(new StatusResponse(HttpStatus.PAYLOAD_TOO_LARGE.value(), "File size exceeds the maximum allowed limit!"));
     }
 }
