0d83a0f082e74d6f0e41c4587f02c39c878d1f09
amankr8 <amankumar141099@gmail.com>
2024-11-26 23:54:16 +0530

    Document constraints added

--- src/main/java/com/furidaweb/server/dto/doc/DocRequestDto.java
+++ src/main/java/com/furidaweb/server/dto/doc/DocRequestDto.java
@@ -1,12 +1,11 @@
 package com.furidaweb.server.dto.doc;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.Date;
 
 @Data
 @AllArgsConstructor
@@ -19,5 +18,6 @@ public class DocRequestDto {
     @NotBlank(message = "Description is required")
     private String desc;
 
+    @NotNull(message = "Document file is required")
     private MultipartFile file;
 }
