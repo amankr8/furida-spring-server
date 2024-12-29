fd678c8e3c7a5428b2296ca76e77d8304262e707
Aman Kumar <amankumar141099@gmail.com>
2024-12-29 22:02:27 +0530

    FWA-60 - Integrate Flyway with Spring Boot for managing migrations

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 -- Insert a default user if it does not exist
-INSERT INTO app_user (u_username, u_email, u_password, u_role)
-SELECT 'root', 'root@furida.in', '$2b$12$bU2d91AuBWWNV6YcxA1.mOjduLvf9iRe3iHHvlFn2f5VJKeGGazXa', 'ADMIN'
-WHERE NOT EXISTS (
-  SELECT 1 FROM app_user WHERE u_username = 'root'
-);
+--INSERT INTO app_user (u_username, u_email, u_password, u_role)
+--SELECT 'root', 'root@furida.in', '$2b$12$bU2d91AuBWWNV6YcxA1.mOjduLvf9iRe3iHHvlFn2f5VJKeGGazXa', 'ADMIN'
+--WHERE NOT EXISTS (
+--  SELECT 1 FROM app_user WHERE u_username = 'root'
+--);
