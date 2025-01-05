3a5d73f8c7a5ac05453cbd322462db57f5e73ea4
Aman Kumar <amankumar141099@gmail.com>
2025-01-05 17:36:11 +0530

    FWA-93 - Spring boot code refactoring

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,9 @@
--- Insert a root user with ADMIN role if it does not exist
-INSERT INTO app_user (u_username, u_email, u_password, u_role)
-VALUES ('root', 'root@furida.in', '$2b$12$bU2d91AuBWWNV6YcxA1.mOjduLvf9iRe3iHHvlFn2f5VJKeGGazXa', 'ADMIN')
+INSERT INTO app_user (u_username, u_email, u_password, u_role, u_create_date)
+VALUES (
+    'root', 
+    'root@furida.in', 
+    '$2b$12$bU2d91AuBWWNV6YcxA1.mOjduLvf9iRe3iHHvlFn2f5VJKeGGazXa', 
+    'ADMIN', 
+    CURRENT_TIMESTAMP
+)
 ON CONFLICT (u_username) DO NOTHING;
